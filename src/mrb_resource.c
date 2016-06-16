e4a8b3490d6fafa11ac5597f06c1580cfcbfdc8c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-06-16 14:31:26 +0900

    Support and apply clang-format

--- src/mrb_resource.c
+++ src/mrb_resource.c
@@ -20,17 +20,15 @@
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-#define SET_MRB_KEY_RUSAGE_MEMBER1(hash, rusage, member)                       \
-  mrb_hash_set(                                                                \
-      mrb, hash, mrb_symbol_value(mrb_intern_cstr(mrb, #member)),              \
-      mrb_float_value(mrb, (double)rusage.ru_utime.tv_sec +                    \
-                               (double)rusage.ru_utime.tv_usec / 1e6));
+#define SET_MRB_KEY_RUSAGE_MEMBER1(hash, rusage, member)                                                               \
+  mrb_hash_set(mrb, hash, mrb_symbol_value(mrb_intern_cstr(mrb, #member)),                                             \
+               mrb_float_value(mrb, (double)rusage.ru_utime.tv_sec + (double)rusage.ru_utime.tv_usec / 1e6));
 
-#define SET_MRB_KEY_RUSAGE_MEMBER2(hash, rusage, member)                       \
-  mrb_hash_set(mrb, hash, mrb_symbol_value(mrb_intern_cstr(mrb, #member)),     \
-               mrb_float_value(mrb, rusage.member));
+#define SET_MRB_KEY_RUSAGE_MEMBER2(hash, rusage, member)                                                               \
+  mrb_hash_set(mrb, hash, mrb_symbol_value(mrb_intern_cstr(mrb, #member)), mrb_float_value(mrb, rusage.member));
 
-static mrb_value mrb_resource_getrusage(mrb_state *mrb, mrb_value self) {
+static mrb_value mrb_resource_getrusage(mrb_state *mrb, mrb_value self)
+{
 
   struct rusage ru;
   int ret = 0;
@@ -41,8 +39,7 @@ static mrb_value mrb_resource_getrusage(mrb_state *mrb, mrb_value self) {
 
   ret = getrusage(who, &ru);
   if (ret < 0) {
-    mrb_raisef(mrb, E_RUNTIME_ERROR, "%S:%S\n", mrb_fixnum_value(errno),
-               mrb_str_new_cstr(mrb, strerror(errno)));
+    mrb_raisef(mrb, E_RUNTIME_ERROR, "%S:%S\n", mrb_fixnum_value(errno), mrb_str_new_cstr(mrb, strerror(errno)));
   }
 
   r = mrb_hash_new(mrb);
@@ -66,7 +63,8 @@ static mrb_value mrb_resource_getrusage(mrb_state *mrb, mrb_value self) {
   return r;
 }
 
-static mrb_value mrb_resource_getrlimit(mrb_state *mrb, mrb_value self) {
+static mrb_value mrb_resource_getrlimit(mrb_state *mrb, mrb_value self)
+{
 
   struct rlimit rl;
   int rc = 0;
@@ -77,8 +75,7 @@ static mrb_value mrb_resource_getrlimit(mrb_state *mrb, mrb_value self) {
 
   rc = getrlimit(resource, &rl);
   if (rc < 0) {
-    mrb_raisef(mrb, E_RUNTIME_ERROR, "%S:%S\n", mrb_fixnum_value(errno),
-               mrb_str_new_cstr(mrb, strerror(errno)));
+    mrb_raisef(mrb, E_RUNTIME_ERROR, "%S:%S\n", mrb_fixnum_value(errno), mrb_str_new_cstr(mrb, strerror(errno)));
   }
 
   array = mrb_ary_new(mrb);
@@ -88,7 +85,8 @@ static mrb_value mrb_resource_getrlimit(mrb_state *mrb, mrb_value self) {
   return array;
 }
 
-static mrb_value mrb_resource_setrlimit(mrb_state *mrb, mrb_value self) {
+static mrb_value mrb_resource_setrlimit(mrb_state *mrb, mrb_value self)
+{
 
   struct rlimit rl;
   int rc = 0;
@@ -107,14 +105,14 @@ static mrb_value mrb_resource_setrlimit(mrb_state *mrb, mrb_value self) {
 
   rc = setrlimit(resource, &rl);
   if (rc < 0) {
-    mrb_raisef(mrb, E_RUNTIME_ERROR, "%S:%S\n", mrb_fixnum_value(errno),
-               mrb_str_new_cstr(mrb, strerror(errno)));
+    mrb_raisef(mrb, E_RUNTIME_ERROR, "%S:%S\n", mrb_fixnum_value(errno), mrb_str_new_cstr(mrb, strerror(errno)));
   }
 
   return mrb_nil_value();
 }
 
-void mrb_mruby_resource_gem_init(mrb_state *mrb) {
+void mrb_mruby_resource_gem_init(mrb_state *mrb)
+{
 
   struct RClass *resource;
   resource = mrb_define_module(mrb, "Resource");
@@ -132,85 +130,69 @@ void mrb_mruby_resource_gem_init(mrb_state *mrb) {
   mrb_define_const(mrb, resource, "RLIMIT_DATA", mrb_fixnum_value(RLIMIT_DATA));
 #endif
 #ifdef RLIMIT_FSIZE
-  mrb_define_const(mrb, resource, "RLIMIT_FSIZE",
-                   mrb_fixnum_value(RLIMIT_FSIZE));
+  mrb_define_const(mrb, resource, "RLIMIT_FSIZE", mrb_fixnum_value(RLIMIT_FSIZE));
 #endif
 #ifdef RLIMIT_LOCKS
-  mrb_define_const(mrb, resource, "RLIMIT_LOCKS",
-                   mrb_fixnum_value(RLIMIT_LOCKS));
+  mrb_define_const(mrb, resource, "RLIMIT_LOCKS", mrb_fixnum_value(RLIMIT_LOCKS));
 #endif
 #ifdef RLIMIT_MEMLOCK
-  mrb_define_const(mrb, resource, "RLIMIT_MEMLOCK",
-                   mrb_fixnum_value(RLIMIT_MEMLOCK));
+  mrb_define_const(mrb, resource, "RLIMIT_MEMLOCK", mrb_fixnum_value(RLIMIT_MEMLOCK));
 #endif
 #ifdef RLIMIT_MSGQUEUE
-  mrb_define_const(mrb, resource, "RLIMIT_MSGQUEUE",
-                   mrb_fixnum_value(RLIMIT_MSGQUEUE));
+  mrb_define_const(mrb, resource, "RLIMIT_MSGQUEUE", mrb_fixnum_value(RLIMIT_MSGQUEUE));
 #endif
 #ifdef RLIMIT_NICE
   mrb_define_const(mrb, resource, "RLIMIT_NICE", mrb_fixnum_value(RLIMIT_NICE));
 #endif
 #ifdef RLIMIT_NOFILE
-  mrb_define_const(mrb, resource, "RLIMIT_NOFILE",
-                   mrb_fixnum_value(RLIMIT_NOFILE));
+  mrb_define_const(mrb, resource, "RLIMIT_NOFILE", mrb_fixnum_value(RLIMIT_NOFILE));
 #endif
 #ifdef RLIMIT_NPROC
-  mrb_define_const(mrb, resource, "RLIMIT_NPROC",
-                   mrb_fixnum_value(RLIMIT_NPROC));
+  mrb_define_const(mrb, resource, "RLIMIT_NPROC", mrb_fixnum_value(RLIMIT_NPROC));
 #endif
 #ifdef RLIMIT_RSS
   mrb_define_const(mrb, resource, "RLIMIT_RSS", mrb_fixnum_value(RLIMIT_RSS));
 #endif
 #ifdef RLIMIT_RTPRIO
-  mrb_define_const(mrb, resource, "RLIMIT_RTPRIO",
-                   mrb_fixnum_value(RLIMIT_RTPRIO));
+  mrb_define_const(mrb, resource, "RLIMIT_RTPRIO", mrb_fixnum_value(RLIMIT_RTPRIO));
 #endif
 #ifdef RLIMIT_RTTIME
-  mrb_define_const(mrb, resource, "RLIMIT_RTTIME",
-                   mrb_fixnum_value(RLIMIT_RTTIME));
+  mrb_define_const(mrb, resource, "RLIMIT_RTTIME", mrb_fixnum_value(RLIMIT_RTTIME));
 #endif
 #ifdef RLIMIT_SIGPENDING
-  mrb_define_const(mrb, resource, "RLIMIT_SIGPENDING",
-                   mrb_fixnum_value(RLIMIT_SIGPENDING));
+  mrb_define_const(mrb, resource, "RLIMIT_SIGPENDING", mrb_fixnum_value(RLIMIT_SIGPENDING));
 #endif
 #ifdef RLIMIT_STACK
-  mrb_define_const(mrb, resource, "RLIMIT_STACK",
-                   mrb_fixnum_value(RLIMIT_STACK));
+  mrb_define_const(mrb, resource, "RLIMIT_STACK", mrb_fixnum_value(RLIMIT_STACK));
 #endif
 #ifdef RLIM_INFINITY
-  mrb_define_const(mrb, resource, "RLIM_INFINITY",
-                   mrb_float_value(mrb, (mrb_float)RLIM_INFINITY));
+  mrb_define_const(mrb, resource, "RLIM_INFINITY", mrb_float_value(mrb, (mrb_float)RLIM_INFINITY));
 #endif
 #ifdef RLIM_SAVED_CUR
-  mrb_define_const(mrb, resource, "RLIM_SAVED_CUR",
-                   mrb_float_value(mrb, (mrb_float)RLIM_SAVED_CUR));
+  mrb_define_const(mrb, resource, "RLIM_SAVED_CUR", mrb_float_value(mrb, (mrb_float)RLIM_SAVED_CUR));
 #endif
 #ifdef RLIM_SAVED_MAX
-  mrb_define_const(mrb, resource, "RLIM_SAVED_MAX",
-                   mrb_float_value(mrb, (mrb_float)RLIM_SAVED_MAX));
+  mrb_define_const(mrb, resource, "RLIM_SAVED_MAX", mrb_float_value(mrb, (mrb_float)RLIM_SAVED_MAX));
 #endif
 #ifdef RUSAGE_SELF
   mrb_define_const(mrb, resource, "RUSAGE_SELF", mrb_fixnum_value(RUSAGE_SELF));
 #endif
 #ifdef RUSAGE_CHILDREN
-  mrb_define_const(mrb, resource, "RUSAGE_CHILDREN",
-                   mrb_fixnum_value(RUSAGE_CHILDREN));
+  mrb_define_const(mrb, resource, "RUSAGE_CHILDREN", mrb_fixnum_value(RUSAGE_CHILDREN));
 #endif
 #ifdef RUSAGE_BOTH
   mrb_define_const(mrb, resource, "RUSAGE_BOTH", mrb_fixnum_value(RUSAGE_BOTH));
 #endif
 #ifdef RUSAGE_THREAD
-  mrb_define_const(mrb, resource, "RUSAGE_THREAD",
-                   mrb_fixnum_value(RUSAGE_THREAD));
+  mrb_define_const(mrb, resource, "RUSAGE_THREAD", mrb_fixnum_value(RUSAGE_THREAD));
 #endif
 
-  mrb_define_module_function(mrb, resource, "getrusage", mrb_resource_getrusage,
-                             MRB_ARGS_REQ(1));
-  mrb_define_module_function(mrb, resource, "getrlimit", mrb_resource_getrlimit,
-                             MRB_ARGS_REQ(1));
-  mrb_define_module_function(mrb, resource, "setrlimit", mrb_resource_setrlimit,
-                             MRB_ARGS_ARG(2, 1));
+  mrb_define_module_function(mrb, resource, "getrusage", mrb_resource_getrusage, MRB_ARGS_REQ(1));
+  mrb_define_module_function(mrb, resource, "getrlimit", mrb_resource_getrlimit, MRB_ARGS_REQ(1));
+  mrb_define_module_function(mrb, resource, "setrlimit", mrb_resource_setrlimit, MRB_ARGS_ARG(2, 1));
   DONE;
 }
 
-void mrb_mruby_resource_gem_final(mrb_state *mrb) {}
+void mrb_mruby_resource_gem_final(mrb_state *mrb)
+{
+}
