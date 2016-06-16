c252b7fab802b1d9401acee682766191261dd976
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-06-16 16:43:10 +0900

    Simpilify the tests

--- test/mrb_resource.rb
+++ test/mrb_resource.rb
@@ -3,17 +3,11 @@
 ##
 
 assert("Resource.getrusage") do
-  before = Resource.getrusage Resource::RUSAGE_SELF
-  10000.times {}
-  after = Resource.getrusage Resource::RUSAGE_SELF
-  assert_true((after[:ru_utime] - before[:ru_utime]) > 0)
+  assert_true(Resource.getrusage(Resource::RUSAGE_SELF)[:ru_utime] > 0)
 end
 
 assert("Resource::Rusage#ru_utime") do
-  before_utime = Resource::Rusage.new(Resource::RUSAGE_SELF).ru_utime
-  10000.times {}
-  after_utime = Resource::Rusage.new(Resource::RUSAGE_SELF).ru_utime
-  assert_true((after_utime - before_utime) > 0)
+  assert_true(Resource::Rusage.new(Resource::RUSAGE_SELF).ru_utime > 0)
 end
 
 # TODO: add other tests
