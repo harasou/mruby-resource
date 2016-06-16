ed8496e2acf4ea53259fcb241f97260e81362214
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-06-16 15:14:49 +0900

    Add test for performance between Resource::Rusage#ru_utime and Resource.getrusage

--- test/mrb_resource.rb
+++ test/mrb_resource.rb
@@ -16,4 +16,17 @@ assert("Resource::Rusage#ru_utime") do
   assert_true((after_utime - before_utime) > 0)
 end
 
+assert("Resource::Rusage#ru_utime vs Resource.getrusage") do
+  a = Resource.getrusage(Resource::RUSAGE_SELF)[:ru_utime]
+  100000.times { Resource.getrusage(Resource::RUSAGE_SELF)[:ru_utime] }
+  b = Resource.getrusage(Resource::RUSAGE_SELF)[:ru_utime] 
+  c = b - a
+
+  a = Resource.getrusage(Resource::RUSAGE_SELF)[:ru_utime]
+  100000.times { Resource::Rusage.new(Resource::RUSAGE_SELF).ru_utime }
+  b = Resource::Rusage.new(Resource::RUSAGE_SELF).ru_utime
+  d = b - a
+
+  assert_true(d < c)
+end
 # TODO: add other tests
