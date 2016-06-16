928770fbeaa3389eac81bac8ebdf18429382cb12
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-06-16 16:28:51 +0900

    Fix test parameter of loop

--- test/mrb_resource.rb
+++ test/mrb_resource.rb
@@ -4,14 +4,14 @@
 
 assert("Resource.getrusage") do
   before = Resource.getrusage Resource::RUSAGE_SELF
-  1000.times {}
+  10000.times {}
   after = Resource.getrusage Resource::RUSAGE_SELF
   assert_true((after[:ru_utime] - before[:ru_utime]) > 0)
 end
 
 assert("Resource::Rusage#ru_utime") do
   before_utime = Resource::Rusage.new(Resource::RUSAGE_SELF).ru_utime
-  1000.times {}
+  10000.times {}
   after_utime = Resource::Rusage.new(Resource::RUSAGE_SELF).ru_utime
   assert_true((after_utime - before_utime) > 0)
 end
